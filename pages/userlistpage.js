4f14ab6a6a22fbe3ee797321c174b0f78cdcbfdc
wanseau <yoohaang01@gmail.com>
2025-10-14 12:03:59 +0800

    Updated userlistpage

--- pages/userlistpage.js
+++ pages/userlistpage.js
@@ -2,13 +2,13 @@ import {View, Text, FlatList, Alert} from "react-native";
 import axios from "axios";
 import {useState, useEffect} from "react";
 import styles from '../styles';
-import { Button } from "react-native-web";
+import { Button } from "react-native";
 
 export default function UserListPage({navigation}){
     const[users, setUsers] = useState([]);
 
     useEffect(() => {
-        axios.get("http://192.168.20.236:8000/registration/api/users/")
+        axios.get("http://127.0.0.1:8000/registration/api/users/")
         .then((res) => {
             setUsers(res.data)
         })
@@ -17,34 +17,36 @@ export default function UserListPage({navigation}){
         })
     },[]
     );
+
     const handleEdit = (user) => {
         navigation.navigate('EditUser', {user});
     }
 
     const handleDelete = (id) => {
-        Alert.alert(
-            "Confirm Delete",
-            "Are you sure you want to delete?",
-            (
-                {text: "Cancel", style: "cancel"},
-                {
-                    text: "Delete",
-                    style: "destructive",
-                    onPress: () => {
-                        axios.delete(`http://192.168.20.236:8000/registration/api/users/${id}/`)
-                        .then(() => {
-                            Alert.alert("Success", "User deleted successfully");
-                        
-                        })
-                        .catch((err) => {
-                            console.error(err);
-                            Alert.alert("Error", "Failed to delete user");
-                        });
-                    }
-                }
-            )
-        );
-    }
+        // Alert.alert(
+        //     "Confirm Delete",
+        //     "Are you sure you want to delete?",
+        //     (
+        //         {text: "Cancel", style: "cancel"},
+        //         {
+        //             text: "Delete",
+        //             style: "destructive",
+        //             onPress: () => {
+        //                 axios.delete(`http://127.0.0.1:8000/registration/api/users/${id}/`)
+        //                 .then(() => {
+        //                     Alert.alert("Success", "User deleted successfully");
+
+        //         })
+        //         .catch((err) => {
+        //             console.error(err);
+        //             Alert.alert("Error", "Failed to delete user");
+        //         });
+        //             }
+        //         }
+        //     )
+        // );
+        axios.delete(`http://127.0.0.1:8000/registration/api/users/${id}/`)
+    };
 
     return(
         <View style={styles.container}>
@@ -64,8 +66,8 @@ export default function UserListPage({navigation}){
                         <Button title="Delete"
                         color="#84240c"
                         onPress={() => handleDelete(item.id)}/>
-                    </View>
-                </View>       
+                    </View>     
+                </View>
             )}/>
         </View>
     )
